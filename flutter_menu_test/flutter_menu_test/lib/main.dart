139e1ef3d84861953d254ab44505200e5fbeab14
Veldhuis94 <43237529+Veldhuis94@users.noreply.github.com>
2020-05-18 19:00:13 +0200

    Update main.dart

--- flutter_menu_test/flutter_menu_test/lib/main.dart
+++ flutter_menu_test/flutter_menu_test/lib/main.dart
@@ -86,7 +86,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     TextFormField(
                       controller: _passwordController,
-                      //obscureText: true,
+                      obscureText: true,
                       validator: (value) {
                         if (value.isEmpty) {
                           return 'Please enter some text';
@@ -122,7 +122,49 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
+  RegisterPage({Key key}) : super(key: key);
+  final String title = "register";
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formkey = GlobalKey<FormState>();
+
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _cityController = TextEditingController();
+  final _streetController = TextEditingController();
+  final _numberController = TextEditingController();
+  final _postalCodeController = TextEditingController();
+  final _numberExtController = TextEditingController();
+
+  Future<void> _performRegister() async {
+    String username = _usernameController.text.trim();
+    String password = _passwordController.text.trim();
+
+    print('login attempt: $username with $password');
+
+    var url = "http://10.0.2.2:8080/user/create";
+    var body = jsonEncode(AuthorizationTokenRequest(username, password));
+
+    http
+        .post(url, headers: {"Content-Type": "application/json"}, body: body)
+        .then((http.Response res) {
+      print("response code" + res.statusCode.toString());
+      print("Response body" + res.body);
+      var m = Message.fromJson(jsonDecode(res.body));
+      if (m.successful) {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => (MyStatefulWidget(userId: m.userId))));
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -130,94 +172,99 @@ class RegisterPage extends StatelessWidget {
           title: Text("Registreer"),
         ),
         body: Center(
-            child: Column(
-          children: <Widget>[
-            TextFormField(
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-              decoration: InputDecoration(labelText: "Username"),
-            ),
-            TextFormField(
-              //obscureText: true,
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-              decoration: InputDecoration(labelText: "Email Address"),
-            ),
-            TextFormField(
-              //obscureText: true,
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-              decoration: InputDecoration(labelText: "Password"),
-            ),
-            TextFormField(
-              //obscureText: true,
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-              decoration: InputDecoration(labelText: "City"),
-            ),
-            TextFormField(
-              //obscureText: true,
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-              decoration: InputDecoration(labelText: "Street"),
-            ),
-            TextFormField(
-              //obscureText: true,
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-              decoration: InputDecoration(labelText: "House Number"),
-            ),
-            TextFormField(
-              //obscureText: true,
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-              decoration: InputDecoration(labelText: "House Number Addition"),
-            ),
-            TextFormField(
-              //obscureText: true,
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-              decoration: InputDecoration(labelText: "Postal Code"),
-            ),
-            RaisedButton(
-                onPressed: () {
-                  print("yuh");
-                },
-                child: Text("Registreer"))
-          ],
-        )));
+            child: Form(
+                key: _formkey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      controller: _usernameController,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(labelText: "Username"),
+                    ),
+                    TextFormField(
+                      controller: _emailController,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(labelText: "Email Address"),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      controller: _passwordController,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(labelText: "Password"),
+                    ),
+                    TextFormField(
+                      controller: _cityController,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(labelText: "City"),
+                    ),
+                    TextFormField(
+                      controller: _streetController,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(labelText: "Street"),
+                    ),
+                    TextFormField(
+                      controller: _numberController,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(labelText: "House Number"),
+                    ),
+                    TextFormField(
+                      controller: _numberExtController,
+                      decoration:
+                          InputDecoration(labelText: "House Number Addition"),
+                    ),
+                    TextFormField(
+                      controller: _postalCodeController,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(labelText: "Postal Code"),
+                    ),
+                    RaisedButton(
+                        onPressed: () {
+                          print("Validating...");
+                          if (_formkey.currentState.validate()) {
+                            print("Validation successful.");
+                            _performRegister();
+                          } else {
+                            print("Validation failed.");
+                          }
+                        },
+                        child: Text("Registreer"))
+                  ],
+                ))));
   }
 }
 
