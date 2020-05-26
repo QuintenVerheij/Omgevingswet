c4648e08811f37a96e39ae2fc015386dd99d2d17
Veldhuis94 <43237529+Veldhuis94@users.noreply.github.com>
2020-05-26 23:00:54 +0200

    Tried to make menu of sorts

---
+++ AR/Assets/LaunchARCamera.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SceneManager;
+
+public class LaunchARCamera : MonoBehaviour
+{
+    void OnMouseUp() {
+        SceneManager.LoadScene("ModelDraaien", LoadSceneMode.single);
+    }
+}
