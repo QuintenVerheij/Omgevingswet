61c938eb23d05ac3f0c06354bea97dee0ebadc47
Veldhuis94 <43237529+Veldhuis94@users.noreply.github.com>
2020-05-27 19:37:55 +0200

    menu attempt

---
+++ AR/Assets/OpenARCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManager;
+using UnityEngine;
+
+
+public class OpenARCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void onMouseUp() {
+        SceneManager.LoadScene("ModelDraaien", LoadSceneModule.single);
+    }
+}
