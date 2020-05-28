f578713d5c826f5e1978f756a87aaf954e71f19e
Naomi van Duijn <Naomivanduijn@live.nl>
2020-05-28 20:06:53 +0200

    Save entered text to JSON
    ingevulde text in de inputvelden wordt in user.txt opgeslagen. als de app gesloten wordt en weer geopend dan onthoud hij de gegevens

---
+++ AR/Assets/Scripts/UI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public InputField userName;
+    public InputField password;
+    public DataManager dataManager;
+
+    void Start()
+    {
+        dataManager.Load();
+        userName.text = dataManager.data.username;
+        password.text = dataManager.data.password;
+    }
+    
+    public void InsertUsername(string text)
+    {
+        dataManager.data.username = text;
+    }
+
+    public void InsertPassword(string text)
+    {
+        dataManager.data.password = text;
+    }
+
+    public void ClickSave()
+    {
+        dataManager.Save();
+    }
+}
