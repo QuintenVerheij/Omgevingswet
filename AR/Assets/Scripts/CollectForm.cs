6c1478228b1f23cd6b435cadd489b2db82321c09
Veldhuis94 <43237529+Veldhuis94@users.noreply.github.com>
2020-06-08 21:05:22 +0200

    register page

---
+++ AR/Assets/Scripts/CollectForm.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectForm : MonoBehaviour
+{
+    Dictionary<String, String> Form = new Dictionary<String, String>();
+    void InputHandler(int which, String input) {
+        Form[which] = input;
+    }
+}
