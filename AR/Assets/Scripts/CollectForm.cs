d057da8f89ef6b5982f1ae5b9570a29251d5e4df
Veldhuis94 <43237529+Veldhuis94@users.noreply.github.com>
2020-06-09 23:29:02 +0200

    register?

--- AR/Assets/Scripts/CollectForm.cs
+++ AR/Assets/Scripts/CollectForm.cs
@@ -4,8 +4,55 @@ using UnityEngine;
 
 public class CollectForm : MonoBehaviour
 {
-    Dictionary<String, String> Form = new Dictionary<String, String>();
-    void InputHandler(int which, String input) {
-        Form[which] = input;
+    Form formCollector;
+    void Start() {
+        formCollector = new Form();
+    }
+    public void InputHandler(int which, string input) {
+        if (which==0) {
+            formCollector.Username = input;
+        }
+        else if (which==1) {
+            formCollector.Password = input;
+        }
+        else if (which==2) {
+            formCollector.Email = input;
+        }
+        else if (which==3) {
+            formCollector.Address.City = input;
+        }
+        else if (which==4) {
+            formCollector.Address.Street = input;
+        }
+        else if (which==5) {
+            formCollector.Address.Number = input;
+        }
+        else if (which==6) {
+            formCollector.Address.NumExt = input;
+        }
+    }
+}
+public class Form {
+    public string Username;
+    public string Password;
+    public string Email;
+    public Address Address;
+    public Form () {
+        Username = "";
+        Password = "";
+        Email = "";
+        Address = new Address();
+    }
+}
+public class Address {
+    public string City;
+    public string Street;
+    public string Number;
+    public string NumExt;
+    public Address () {
+        City = "";
+        Street = "";
+        Number = "";
+        NumExt = "";
     }
 }
