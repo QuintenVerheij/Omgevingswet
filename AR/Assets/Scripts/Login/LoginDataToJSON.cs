a577a5bdca96e0fbed763b78d64d175633599200
Naomi van Duijn <Naomivanduijn@live.nl>
2020-06-10 10:36:25 +0200

    Login
    login werkt maar hij herleid nog niet naar een andere scene wanneer dit gelukt is.

--- AR/Assets/Scripts/Login/LoginDataToJSON.cs
+++ AR/Assets/Scripts/Login/LoginDataToJSON.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LoginDataToJSON
 {
-    public string username = "";
+    public string mail = "";
     public string password = "";
 }
 
---
+++ AR/Assets/Scripts/Login/LoginRequest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class LoginRequest : MonoBehaviour
+{
+    public InputField userName;
+    public InputField password;
+
+    public LoginDataToJSON data;
+
+    public Button loginButton;
+
+    public void ClickLogin()
+    {
+        StartCoroutine(Login());
+    }
+
+    IEnumerator Login()
+    {
+        string url = "localhost:8080/auth/login";
+
+        data = new LoginDataToJSON();
+        data.mail = userName.text;
+        data.password = password.text;
+        string body = JsonUtility.ToJson(data);
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(body);
+
+        UnityWebRequest www = new UnityWebRequest(url, "POST");
+        www.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+        www.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        www.SetRequestHeader("Content-Type", "application/json");
+        
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(body);
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Login Complete");
+        }
+        
+    }
+
+    public void CheckInputFields()
+    {
+        loginButton.interactable = (userName.text.Length >= 1 && password.text.Length >= 1);
+    }
+
+}
