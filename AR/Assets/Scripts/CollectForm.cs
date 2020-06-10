3f81fceabe6282da31c60cc0bbb0cab23e8da5d8
Veldhuis94 <43237529+Veldhuis94@users.noreply.github.com>
2020-06-10 15:58:01 +0200

    input handling

--- AR/Assets/Scripts/CollectForm.cs
+++ AR/Assets/Scripts/CollectForm.cs
@@ -1,12 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using System;
 
 public class CollectForm : MonoBehaviour
 {
     Form formCollector;
+    public InputField userInputField;
+    public InputField passInputField;
+    public InputField mailInputField;
+    public InputField cityInputField;
+    public InputField strtInputField;
+    public InputField numbInputField;
+    public InputField nextInputField;
+    public InputField postInputField;
+    public Button submitButton;
     void Start() {
         formCollector = new Form();
+        userInputField.onEndEdit.AddListener(delegate {InputHandler(0, userInputField.text);});
+        passInputField.onEndEdit.AddListener(delegate {InputHandler(1, passInputField.text);});
+        mailInputField.onEndEdit.AddListener(delegate {InputHandler(2, mailInputField.text);});
+        cityInputField.onEndEdit.AddListener(delegate {InputHandler(3, cityInputField.text);});
+        strtInputField.onEndEdit.AddListener(delegate {InputHandler(4, strtInputField.text);});
+        numbInputField.onEndEdit.AddListener(delegate {InputHandler(5, numbInputField.text);});
+        nextInputField.onEndEdit.AddListener(delegate {InputHandler(6, nextInputField.text);});
+        postInputField.onEndEdit.AddListener(delegate {InputHandler(7, postInputField.text);});
+        submitButton.onClick.AddListener(delegate {submit();});
     }
     public void InputHandler(int which, string input) {
         if (which==0) {
@@ -19,40 +39,56 @@ public class CollectForm : MonoBehaviour
             formCollector.Email = input;
         }
         else if (which==3) {
-            formCollector.Address.City = input;
+            formCollector.City = input;
         }
         else if (which==4) {
-            formCollector.Address.Street = input;
+            formCollector.Street = input;
         }
         else if (which==5) {
-            formCollector.Address.Number = input;
+            formCollector.Number = input;
         }
         else if (which==6) {
-            formCollector.Address.NumExt = input;
+            formCollector.NumExt = input;
         }
+        else if (which==7) {
+            formCollector.PostCode = input;
+        }
+    }
+    public UserCreateInput submit() {
+        UserCreateInput res = new UserCreateInput(
+            formCollector.Username,
+            formCollector.Password,
+            formCollector.Email,
+            new AddressCreateInput(
+                formCollector.City,
+                formCollector.Street,
+                int.Parse(formCollector.Number),
+                formCollector.NumExt,
+                formCollector.PostCode
+            )
+        );
+        Debug.Log(res.ToString());
+        return res;
     }
 }
 public class Form {
     public string Username;
     public string Password;
     public string Email;
-    public Address Address;
-    public Form () {
-        Username = "";
-        Password = "";
-        Email = "";
-        Address = new Address();
-    }
-}
-public class Address {
     public string City;
     public string Street;
     public string Number;
     public string NumExt;
-    public Address () {
+    public string PostCode;
+    public Form () {
+        Username = "";
+        Password = "";
+        Email = "";
         City = "";
         Street = "";
         Number = "";
         NumExt = "";
+        PostCode = "";
     }
 }
+
