fb149de4d888320b99db41d75e697b1a16ef2716
Veldhuis94 <43237529+Veldhuis94@users.noreply.github.com>
2020-06-12 15:29:33 +0200

    change string username

--- AR/Assets/Scripts/ReadProfile.cs
+++ AR/Assets/Scripts/ReadProfile.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ReadProfile : MonoBehaviour
+{
+    MessageWithItem<UserOutput> res;
+    public Text username;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine (GetUserInfo());
+    }
+
+    IEnumerator GetUserInfo() {
+        UnityWebRequest net = UnityWebRequest.Get("localhost:8080/user/read");
+        yield return net.SendWebRequest();
+        if (net.isNetworkError || net.isHttpError) {
+            Debug.Log(net.error);
+            username.text = net.error;
+        } else {
+            Debug.Log(net.downloadHandler.text);
+            res = (MessageWithItem<UserOutput>) MessageWithItem<UserOutput>.fromJson(net.downloadHandler.text);
+            username.text = res.item.username;
+        }
+    }
+}
