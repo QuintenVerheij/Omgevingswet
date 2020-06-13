cf1fc7f4da90d2a29ffe1c96b9a6bbdba908058b
Veldhuis94 <43237529+Veldhuis94@users.noreply.github.com>
2020-06-13 22:45:06 +0200

    profile picture start

--- AR/Assets/Scripts/ReadProfile.cs
+++ AR/Assets/Scripts/ReadProfile.cs
@@ -6,20 +6,39 @@ using UnityEngine.UI;
 
 public class ReadProfile : MonoBehaviour
 {
-    MessageWithItem<UserOutput> res;
+    MessageWithItem<UserOutput> usernameres;
     public Text username;
     currentUser user;
     AuthorizationToken token;
     AuthorizedAction<int> action;
+    RawImage PictureFinal;
+    Texture2D profilePic;
     // Start is called before the first frame update
     void Start()
     {
         user = new currentUser();
         token = new AuthorizationToken(user.readToken());
         action = new AuthorizedAction<int>(token, user.readUserId());
+        profilePic = new Texture2D(300, 300);
         StartCoroutine (GetUserInfo());
+        StartCoroutine (GetProfilePic(profilePic));
     }
 
+    IEnumerator GetProfilePic(Texture2D texture) {
+        UnityWebRequest ppic = new UnityWebRequest("localhost:8080/user/img/" + user.readUserId(), "GET");
+        ppic.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
+        yield return ppic.SendWebRequest();
+        if (ppic.isNetworkError || ppic.isHttpError) {
+            Debug.Log(ppic.error);
+        } else {
+            Debug.Log(ppic.downloadHandler.text);
+            byte[] profileres = System.Text.Encoding.UTF8.GetBytes(ppic.downloadHandler.text);
+            texture.LoadRawTextureData(profileres);
+            texture.Apply();
+            texture.EncodeToJPG();
+            PictureFinal.texture = texture;
+        }
+    }
     IEnumerator GetUserInfo() {
         UnityWebRequest net = new UnityWebRequest("localhost:8080/user/read", "POST");
         net.uploadHandler = (UploadHandler) new UploadHandlerRaw(action.toJsonRaw());
@@ -31,9 +50,9 @@ public class ReadProfile : MonoBehaviour
             username.text = net.error;
         } else {
             Debug.Log(net.downloadHandler.text);
-            res = (MessageWithItem<UserOutput>) MessageWithItem<UserOutput>.fromJson(net.downloadHandler.text);
-            if(res.message.successful) {
-                username.text = res.item.username;
+            usernameres = (MessageWithItem<UserOutput>) MessageWithItem<UserOutput>.fromJson(net.downloadHandler.text);
+            if(usernameres.message.successful) {
+                username.text = usernameres.item.username;
             } else {
                 username.text = "[Niet ingelogd]";
             }
