4b67d4cd17595f175f37b719a82d345b98c1c5d8
Naomi van Duijn <Naomivanduijn@live.nl>
2020-06-13 19:06:09 +0200

    marker op map met markerdata

--- AR/Assets/Scripts/Map/ClickMarker.cs
+++ AR/Assets/Scripts/Map/ClickMarker.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class ClickMarker : MonoBehaviour
 {
-
+    private MarkerData _data;
     public GameObject canvas;
+    public void SetMarkerData(MarkerData data)
+    {
+        _data = data;
+    }
     public void OnMouseUp()
     {
         GameObject.Find("PopUpMessage").GetComponent<Canvas>().enabled = true;
+        Debug.Log(_data.id);
+        Debug.Log(_data.createdAt);
 
-    //Load in image from backend
-    //set image of image child from canvas to image:
-    //RawImage rawImage = canvas.gameObject.GetComponentInChildren<RawImage>();
-    //rawImage.texture = texture;
-}
+        //Load in image from backend
+        //set image of image child from canvas to image:
+        //RawImage rawImage = canvas.gameObject.GetComponentInChildren<RawImage>();
+        //rawImage.texture = texture;
+    }
 }
