14fe2425d01329eb8fa9d854fae8fb97f83ea867
Quinten <Q.verheij@hotmail.com>
2020-06-13 18:33:55 +0200

    Camera functionality added

--- AR/Assets/Scripts/Profile/ReadProfile.cs
+++ AR/Assets/Scripts/Profile/ReadProfile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.IO;
 
 public class ReadProfile : MonoBehaviour, IPointerClickHandler
 {
@@ -12,10 +13,14 @@ public class ReadProfile : MonoBehaviour, IPointerClickHandler
     currentUser user;
     AuthorizationToken token;
     AuthorizedAction<int> action;
+    private Rect ChoiceWindow = new Rect((Screen.width / 2) - 400, Screen.height / 2 - 600, 800, 450);
+    private Rect CamWindow = new Rect(0, 0, Screen.width, Screen.height);
 
-    private Rect WindowRect = new Rect((Screen.width / 2) - 400, Screen.height / 2 - 600, 800, 450);
+    private WebCamTexture webCamTexture;
 
+    public Texture webcamButton;
     public bool isProfilePicTapped;
+    private bool showCam;
     // Start is called before the first frame update
     void Start()
     {
@@ -61,7 +66,11 @@ public class ReadProfile : MonoBehaviour, IPointerClickHandler
     {
         if (isProfilePicTapped)
         {
-            WindowRect = GUI.Window(0, WindowRect, picChoices, "Main Menu");
+            ChoiceWindow = GUI.Window(0, ChoiceWindow, picChoices, "Choose a source");
+        }
+        if (showCam)
+        {
+            CamWindow = GUI.Window(1, CamWindow, CamTexture, "Camera");
         }
     }
 
@@ -72,11 +81,44 @@ public class ReadProfile : MonoBehaviour, IPointerClickHandler
         if (GUI.Button(new Rect(50, 50, 700, 150), "Camera", buttons))
         {
             Debug.Log("Camera");
+            webCamTexture = new WebCamTexture();
+            GetComponent<Renderer>().material.mainTexture = webCamTexture; //Add Mesh Renderer to the GameObject to which this script is attached to
+            webCamTexture.Play();
+
+            showCam = true;
+            isProfilePicTapped = false;
         }
         if (GUI.Button(new Rect(50, 250, 700, 150), "Gallery", buttons))
         {
             Debug.Log("Gallery");
         }
 
+    }
+
+    private void CamTexture(int id)
+    {
+        GUI.DrawTexture(new Rect(0, 0, CamWindow.width, CamWindow.height), webCamTexture, ScaleMode.ScaleToFit);
+        GUIStyle buttons = new GUIStyle("button");
+        buttons.fontSize = 40;
+        if (GUI.Button(new Rect((CamWindow.width / 2 - 100), CamWindow.height - 200, 200, 200), webcamButton, buttons))
+        {
+            StartCoroutine(TakePhoto());
+        }
+    }
+
+    IEnumerator TakePhoto()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Texture2D photo = new Texture2D(webCamTexture.width, webCamTexture.height);
+        photo.SetPixels(webCamTexture.GetPixels());
+        photo.Apply();
+
+        //Encode to a PNG
+        byte[] bytes = photo.EncodeToPNG();
+        //Write out the PNG. Of course you have to substitute your_path for something sensible
+        File.WriteAllBytes("Assets/Resources/" + user.readUserId() + "_Profile.png", bytes);
+        showCam = false;
+        //TODO: Upload image to backend, reload profile page
     }
 }
