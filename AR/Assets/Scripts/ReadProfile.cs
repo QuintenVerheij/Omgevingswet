5f169c4bbf041d843646659994e59b06f0b24ce4
Veldhuis94 <43237529+Veldhuis94@users.noreply.github.com>
2020-06-13 13:21:32 +0200

    authjson

--- AR/Assets/Scripts/ReadProfile.cs
+++ AR/Assets/Scripts/ReadProfile.cs
@@ -8,21 +8,21 @@ public class ReadProfile : MonoBehaviour
 {
     MessageWithItem<UserOutput> res;
     public Text username;
-    string[] authJson = {
-        "auth", {
-            "token","0cb6ebb9511aa521864d39542d0cce4e4a29886c85cf926afd016f147f6a877675aaef0fab332d0ca834cac07ce2287631726d4fd968485f06b384c039448b85"
-        },
-        "input",0
-    };
+    currentUser user;
+    AuthorizationToken token;
+    AuthorizedAction<int> action;
     // Start is called before the first frame update
     void Start()
     {
+        user = new currentUser();
+        token = new AuthorizationToken(user.readToken());
+        action = new AuthorizedAction<int>(token, user.readUserId());
         StartCoroutine (GetUserInfo());
     }
 
     IEnumerator GetUserInfo() {
         UnityWebRequest net = new UnityWebRequest("localhost:8080/user/read", "POST");
-        net.uploadHandler = (UploadHandler) new UploadHandlerRaw(authJson);
+        net.uploadHandler = (UploadHandler) new UploadHandlerRaw(action.toJsonRaw());
         net.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
         net.SetRequestHeader("Content-Type", "application/json");
         yield return net.SendWebRequest();
