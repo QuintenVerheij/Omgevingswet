dbba4368b646395bd77583be1f674536e605388c
Naomi van Duijn <Naomivanduijn@live.nl>
2020-06-16 16:20:03 +0200

    viewprofile

--- AR/Assets/Scripts/Map/ClickMarker.cs
+++ AR/Assets/Scripts/Map/ClickMarker.cs
@@ -52,7 +52,25 @@ public class ClickMarker : MonoBehaviour
         _popUpMessage.GetComponentInChildren<RawImage>().texture = newTexture;
         Debug.Log(_data);
         _popUpMessage.GetComponent<ViewOtherModel>().SetModelID(_data.id);
-        _popUpMessage.GetComponent<LoadSceneWithUserId>().setUserId(_data.userid);
+        _popUpMessage.GetComponent<ClickViewProfile>().SetUserID(_data.userid);
+
+    }
+    public void OnTouchUp()
+    {
+        _popUpMessage = GameObject.Find("PopUpMessage");
+        _popUpMessage.GetComponent<Canvas>().enabled = true;
+        Debug.Log(_data.id);
+        Debug.Log(_data.createdAt);
+
+        //Load in image from backend
+        //set image of image child from canvas to image:
+        Texture2D newTexture = new Texture2D(650, 550, TextureFormat.ARGB4444, false);
+        newTexture.LoadImage(_data.preview);
+        newTexture.Apply();
+        _popUpMessage.GetComponentInChildren<RawImage>().texture = newTexture;
+        Debug.Log(_data);
+        _popUpMessage.GetComponent<ViewOtherModel>().SetModelID(_data.id);
+        _popUpMessage.GetComponent<ClickViewProfile>().SetUserID(_data.userid);
 
     }
 
---
+++ AR/Assets/Scripts/Map/ClickViewProfile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickViewProfile : MonoBehaviour
+{
+    private int _userID;
+    public Button button;
+    public void SetUserID(int userID)
+    {
+        this._userID = userID;
+    }
+
+    void start()
+    {
+        LoadSceneWithUserId sl = new LoadSceneWithUserId();
+        button.onClick.AddListener(() => sl.SceneLoader(_userID));
+    }
+}
