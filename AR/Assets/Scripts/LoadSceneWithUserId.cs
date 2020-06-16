01386751806b9ca910b94e8036ba72b3af0def0e
Naomi van Duijn <Naomivanduijn@live.nl>
2020-06-16 15:50:35 +0200

    Auto stash before merge of "MapBoxTesting" and "origin/MapBoxTesting"

--- AR/Assets/Scripts/LoadSceneWithUserId.cs
+++ AR/Assets/Scripts/LoadSceneWithUserId.cs
@@ -5,19 +5,9 @@ using UnityEngine;
 
 public class LoadSceneWithUserId : MonoBehaviour
 {
-    private int _userId;
-    public void setUserId(int userId)
-    {
-        _userId = userId;
-    }
     public void SceneLoader(int userId)
     {
         ReadProfile.crossedId = userId;
-        SceneManager.LoadScene(3);
-    }
-    public void SceneLoader()
-    {
-        ReadProfile.crossedId = _userId;
         SceneManager.LoadScene(3);
     }
 }
